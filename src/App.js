cc4becfb6b101c06592c5f1cf12a5e1fc83c3fee
dkjamerson55 <derekkj_55@hotmail.com>
2023-04-26 14:08:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Nav from './Nav'
+import Login from './loginForm'
+
+function App() {
+    return <div className="App">
+        <Nav></Nav>
+        <Login></Login>
+    </div>
+}
+
+export default App
---
+++ src/loginForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+export default class Login extends Component{
+    render(){
+        return(
+            <section className='loginForm'>
+                <h3>Log In</h3>
+                <form className='userInput'>
+                    <div className='userName'>
+                        <label className='form_label' for="userName">User Name:</label>
+                        <input className='form_input' type='text' id='userName' placeholder='user@email'></input>
+                    </div>
+
+                    <div className='passwordInput'>
+                        <label className='form_label' for="password">Password:</label>
+                        <input className='form_input' type='text' id='password' placeholder='*****'></input>
+                    </div>
+
+                </form>
+            </section>
+        )
+    }
+}
